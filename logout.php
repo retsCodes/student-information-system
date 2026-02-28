4adf4b7ef92c2d8971160c556e4c141bf52c8c50
Rets <richcuizon2@gmail.com>
2026-02-28 18:10:11 +0800

    add of profile picture

--- logout.php
+++ logout.php
@@ -5,6 +5,8 @@ if (checkSession()) {
     // Log activity
     logActivity($_SESSION['user_id'], 'Logout', 'User logged out');
     
+    unset($_SESSION['profile_picture']);
+    
     // Destroy session
     session_destroy();
 }
